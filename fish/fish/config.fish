b7719039d89ee9f019662959b2f5a3df0108d88c
Sofia Nieves <sofia@qwfp.net>
2020-05-13 20:14:38 -0400

    Addid vi keybinds to fish

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,3 +1,5 @@
-set GOPATH $HOME/go
-set PATH $HOME/bin $HOME/.local/bin /usr/local/bin /usr/bin /usr/sbin /bin
 set fish_greeting ""
+fish_vi_key_bindings
+
+set GOPATH $HOME/go
+set -a PATH $HOME/bin $HOME/.local/bin
