d02298758fd62b916b9444d0910776e0bf42ff06
Sofia Nieves <sofia+git@qwfp.net>
2026-09-02 18:43:04 -0400

    latest fish config

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,7 +1,13 @@
 set fish_greeting ""
 fish_vi_key_bindings
 
+# Executable paths
 set -p PATH $HOME/go/bin
 set -p PATH $HOME/bin $HOME/.local/bin
+
+# Local man pages
+set -p MANPATH $HOME/.local/share/man
+
+source $HOME/.config/fish/functions/workschoolbalance.fish
 
 #set -x XDG_DATA_DIRS $HOME/.local/share/flatpak/exports/share
